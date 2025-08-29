d9636ffcc69eb4d4c88f8377b4b15c709e48a942
belajar-vue-2 <aryaadi229@gmail.com>
2025-08-29 19:48:21 +0700

    feat: restructure views by removing welcome page, adding main layout component, and implementing item show page

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('main');
+
+Route::get('/items/1', function () {
+    return view('show');
+})->name('item.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
